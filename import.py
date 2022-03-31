707a3c76d1cb5d47f8eb1e0c4225446e27a2166e
Alex Le Page <alexlepage777@gmail.com>
2022-03-31 14:49:08 +0200

    variation du code 1

--- import.py
+++ import.py
@@ -2,12 +2,27 @@ import requests
 import json
 import unicodedata
 
+
+# le but de ceci est de venir convertir les données que l'on récupère dans un format qui est égal ou proche de ce que l'on a fait dans le projet questionnaire
+# pour chaque lien on va pouvoir extraire 3 quiz en fonction des niveau de diffcultés
+# 1ere chose  àfaire  :reparer ce code : pourquoi ça ne marche plus. Si on fait "ls" on voit tous les fichiers "json" qui ont servie pour l'import. Si on souhaite
+# que les fichiers "json" soit généré dans le même fichier que le script
+# une fois que l'on a les fichiers au format json dans notre import, l'idée va être de pouvoir
+# les ouvrir dans notre onglet questionnaire
+
+
+#open_quizz_db_data = (
+     #   ("Animaux", "Les chats", "https://www.kiwime.com/oqdb/files/1050288832/OpenQuizzDB_050/openquizzdb_50.json"),
+    #    ("Arts", "Musée du Louvre", "https://www.kiwime.com/oqdb/files/1086665427/OpenQuizzDB_086/openquizzdb_86.json"),
+   #     ("Bande dessinnée", "Tintin", "https://www.kiwime.com/oqdb/files/2124627594/OpenQuizzDB_124/openquizzdb_124.json"),
+  #      ("Gastronomie", "Chocolat", "https://www.kiwime.com/oqdb/files/1019678723/OpenQuizzDB_019/openquizzdb_19.json"),
+ #       ("Cinéma", "Star wars", "https://www.kiwime.com/oqdb/files/1090683544/OpenQuizzDB_090/openquizzdb_90.json"),
+#)
+
+
 open_quizz_db_data = (
-    ("Animaux", "Les chats", "https://www.kiwime.com/oqdb/files/1050828847/OpenQuizzDB_050/openquizzdb_50.json"),
-    ("Arts", "Musée du Louvre", "https://www.kiwime.com/oqdb/files/1086624389/OpenQuizzDB_086/openquizzdb_86.json"),
-    ("Bande dessinnée", "Tintin", "https://www.kiwime.com/oqdb/files/2124627384/OpenQuizzDB_124/openquizzdb_124.json"),
-    ("Cinéma", "Alien", "https://www.kiwime.com/oqdb/files/3241454997/OpenQuizzDB_241/openquizzdb_241.json"),
-    ("Cinéma", "Star wars", "https://www.kiwime.com/oqdb/files/1090993427/OpenQuizzDB_090/openquizzdb_90.json"),
+        ("Animaux", "Les chats", "https://www.kiwime.com/oqdb/files/1050288832/OpenQuizzDB_050/openquizzdb_50.json"),
+        ("Animaux", "Les chats", "https://www.kiwime.com/oqdb/files/1050288832/OpenQuizzDB_050/openquizzdb_50.json")
 )
 
 
@@ -19,17 +34,30 @@ def get_quizz_filename(categorie, titre, difficulte):
     return strip_accents(categorie).lower().replace(" ", "") + "_" + strip_accents(titre).lower().replace(" ", "") + "_" + strip_accents(difficulte).lower().replace(" ", "") + ".json"
 
 
-def generate_json_file(categorie, titre, url):
+def generate_json_file(categorie, titre, url):  # cette fonction permet de prendre les données texte intiale de les
+    # traiter en les mettant dans le bon ordre et renvoyer un fichier "texte" global" "out_json"
     out_questionnaire_data = {"categorie": categorie, "titre": titre, "questions": []}
+    print(out_questionnaire_data)
     out_questions_data = []
     response = requests.get(url)
-    data = json.loads(response.text)
-    all_quizz = data["quizz"]["fr"]
+  #  print("LES DATAS SERIALISES (sous forme de texte) SONT")
+   # print(response.text)  # il y'a 30 questions
+   # print(type(response.text))
+    data = json.loads(response.text)  # on désérialise les données "reponse". on passe : text à dico
+   # print(type(data))
+  #  print("LES DATAS DESERIALISES (sous forme de dictionnaire) SONT")
+   # print(data)
+    all_quizz = data["quizz"]["fr"]  # correspond au différents quizz : il y'a 3 : car 3 niveaux de difficulté
+  #  print("LES DATAS 'all_quizz' SONT")
+  #  print(all_quizz)
+  #  print(len(all_quizz))
     for quizz_title, quizz_data in all_quizz.items():
         out_filename = get_quizz_filename(categorie, titre, quizz_title)
-        print(out_filename)
+       # print(out_filename)  # on écrit le nom de sortie
         out_questionnaire_data["difficulte"] = quizz_title
-        for question in quizz_data:
+       # print("LES DATAS 'out_questionnaire_data' SONT")
+       # print(quizz_title)
+        for question in quizz_data:  # "quizz_data" correspond au 10 question débutant, puis au 10 question confirmé, e
             question_dict = {}
             question_dict["titre"] = question["question"]
             question_dict["choix"] = []
@@ -37,14 +65,60 @@ def generate_json_file(categorie, titre, url):
                 question_dict["choix"].append((ch, ch==question["réponse"]))
             out_questions_data.append(question_dict)
         out_questionnaire_data["questions"] = out_questions_data
-        out_json = json.dumps(out_questionnaire_data)
+        out_json = json.dumps(out_questionnaire_data)  # on sérialise les données "out_questionnaire_data" -> texte
+       # print("LES DATAS 'out_json' (en mode texte)  SONT")
+        print(out_json)  # on additionne les données du questionnaire débutant puis du cofirmé, puis du expert
 
+        print(type(out_json))
+        # le "out_json" final est un fichier texte contenant toute les données des 3 questionnaires
+        # on écrit toutes ces données dans un fichier texte appelé "out_filename"
+        # out_filename = get_quizz_filename(categorie, titre, quizz_title) avec ceci on va dans la fonction qui permet
+        # d'obtenir un fichier "filename"
         file = open(out_filename, "w")
         file.write(out_json)
         file.close()
-        print("end")
+        # là j'ai décidé d'écrire mes données sur un fichier texte pour voir ce que cela donne réellement
+        file = open("mon_fichier_txt_avec_question", "w")
+        file.write(out_json)
+        file.close()
 
+       # print("end")
+  #  print(out_questions_data)
+  #  print(out_questions_data[0])  # question 1
+  #  print(out_questionnaire_data)
+   # print(out_questions_data[1])  # question 2
+   # print(out_questions_data[3])  # question 3
+   # print(out_questions_data[4])  # question 4
+   # print(out_questions_data[5])  # question 5
+   # print(out_questions_data[6])  # question 6
+   # print(out_questions_data[7])  # question 7
+   # print(out_questions_data[8])  # question 8
+    # il y'a ue bon nom de question dans ce questionnaire : sous forme d'un dictionnaire
+   # dico_calssique = out_questions_data[0]
+   # dico_calssique = json.loads(t)
+    print()
 
 for quizz_data in open_quizz_db_data:
     generate_json_file(quizz_data[0], quizz_data[1], quizz_data[2])
+
+
+
+# ETAPE 1 : debugguer le code. L'idée est que le code que tourne
+
+# le problèmeque l'on avaut rencontré est que certains liens n'étais plus à jour
+# "https://www.kiwime.com/oqdb/files/1019678723/OpenQuizzDB_019/openquizzdb_19.json"  : OK : chocolat
+#  " https://www.kiwime.com/oqdb/files/1050288832/OpenQuizzDB_050/openquizzdb_50.json" : ok : chat
+#  "https://www.kiwime.com/oqdb/files/1086665427/OpenQuizzDB_086/openquizzdb_86.json"  : Ok : musée du louvre
+#  "https://www.kiwime.com/oqdb/files/2124627594/OpenQuizzDB_124/openquizzdb_124.json"  : ok : tintin
+#  https://www.kiwime.com/oqdb/files/1090683544/OpenQuizzDB_090/openquizzdb_90.json  : ok : starwars
+
+# ETAPE 2 : on va charger un fichier. obtenir les données, les désérialisées et faire tourner le fichier "qestionnaire"
+# à partir des données qui sont dans le fichier
+# en fait le fichier chat par exemple est un questionnaire à part entière
+
+
+
+# PROBLEME RENCONTRE :
+# fair ele lien entre mes deux fichiers ".py" afin de lui transmettre les questionnaires que j'ai recu d'internet à la
+# partie sur les questionnaire qui va le traiter comme je le souhaite.
 
--- questionnaire.py
+++ questionnaire.py
@@ -1,3 +1,11 @@
+import requests
+import json
+import unicodedata
+
+
+# CES IMPORTS au-dessus sont du au copier / coller du fichier "import.py"
+
+
 # PROJET QUESTIONNAIRE V3 : POO
 #
 # - Pratiquer sur la POO
@@ -19,6 +27,12 @@
 #    - lancer()
 #
 
+
+# dans ce questionnaire l'iéde est que : au lien d'utiliser des données en dur comme c'est le cas acutellement,
+# on va charger un fichier "json" récupéré dans le fichier "import" le désérialiser et l'utiliser. Au débu on lui passera le fichier en dur,
+# puis on rajoutera la possiblité de lui passer le fichier en ligne de commande
+
+
 class Question:
     def __init__(self, titre, choix, bonne_reponse):
         self.titre = titre
@@ -30,26 +44,26 @@ class Question:
         q = Question(data[2], data[0], data[1])
         return q
 
-    def poser(self):
-        print("QUESTION")
+    def poser(self):  # cette fonction est appelé à chaque nouvelle question
+        # print("QUESTION")
         print("  " + self.titre)
         for i in range(len(self.choix)):
-            print("  ", i+1, "-", self.choix[i])
+            print("  ", i + 1, "-", self.choix[i])
 
         print()
         resultat_response_correcte = False
         reponse_int = Question.demander_reponse_numerique_utlisateur(1, len(self.choix))
-        if self.choix[reponse_int-1].lower() == self.bonne_reponse.lower():
+        if self.choix[reponse_int - 1].lower() == self.bonne_reponse.lower():
             print("Bonne réponse")
             resultat_response_correcte = True
         else:
             print("Mauvaise réponse")
-            
+
         print()
         return resultat_response_correcte
 
     def demander_reponse_numerique_utlisateur(min, max):
-        reponse_str = input("Votre réponse (entre " + str(min) + " et " + str(max) + ") :")
+        reponse_str = input("Votre réponse (entre " + str(min) + " et " + str(max) + ") : ")
         try:
             reponse_int = int(reponse_str)
             if min <= reponse_int <= max:
@@ -59,14 +73,25 @@ class Question:
         except:
             print("ERREUR : Veuillez rentrer uniquement des chiffres")
         return Question.demander_reponse_numerique_utlisateur(min, max)
-    
+
+
 class Questionnaire:
+    numero_question = 0
+
     def __init__(self, questions):
         self.questions = questions
 
     def lancer(self):
+        print("CATEGORIE : " + categorie)
+        print()
+        print("TITRE : " + titre)
+        print()
+        #  print(len(self.questions))
         score = 0
         for question in self.questions:
+            self.numero_question += 1
+            print("QUESTION N°" + str(self.numero_question) + " / " + str(
+                len(self.questions)))  # on met ce "print" ici pour avoir le numéro de la question
             if question.poser():
                 score += 1
         print("Score final :", score, "sur", len(self.questions))
@@ -88,12 +113,90 @@ lancer_questionnaire(questionnaire)"""
 # q = Question.FromData(data)
 # print(q.__dict__)
 
-Questionnaire(
-    (
-    Question("Quelle est la capitale de la France ?", ("Marseille", "Nice", "Paris", "Nantes", "Lille"), "Paris"), 
-    Question("Quelle est la capitale de l'Italie ?", ("Rome", "Venise", "Pise", "Florence"), "Rome"),
-    Question("Quelle est la capitale de la Belgique ?", ("Anvers", "Bruxelles", "Bruges", "Liège"), "Bruxelles")
-    )
+# Questionnaire(
+#   (
+#  Question("Quelle est la capitale de la France ?", ("Marseille", "Nice", "Paris", "Nantes", "Lille"), "Paris"),
+# Question("Quelle est la capitale de l'Italie ?", ("Rome", "Venise", "Pise", "Florence"), "Rome")
+# Question("Quelle est la capitale de la Belgique ?", ("Anvers", "Bruxelles", "Bruges", "Liège"), "Bruxelles")
+# )
+# ).lancer()
+
+
+# ce que l'on  obtient à la suite du "import" sur le chat par ex
+#  [
+#  {   'titre': "À quelle classe d'animaux vertébrés regroupant près de 5.400 espèces appartient le chat ?",
+# 'choix': [('Oiseaux', False), ('Poissons', False), ('Mammifères', True), ('Reptiles', False)]   },
+#  {'titre': 'Lorsque vous apercevez votrchat faire le gros dos, il est probablement...',
+#  'choix': [('Malade', False), ('En chasse', False), ('Effrayé', True), ('Content', False)]}
+
+# Ceci est sous forme d'un dico
+
+# On va charger un fichier json, désérilaiser les données et faire tourner le qestionnaire à partir des données qui
+# sont dans un fichier. C'est à dire que l'on va récupérer des données texte et on va les mettre sous forme d'un dico
+
+
+# Questionnaire(
+#   (
+#  Question(questions[1]['titre'], (questions[1]['choix'][0][0], questions[1]['choix'][1][0], questions[1]['choix'][2][0], questions[1]['choix'][3][0]), questions[1]['choix'][2][0]),
+# Question(questions[2]['titre'], (questions[2]['choix'][0][0], questions[2]['choix'][1][0], questions[2]['choix'][2][0], questions[2]['choix'][3][0]), questions[2]['choix'][2][0]),
+# Question("Quelle est la capitale de l'Italie ?", ("Rome", "Venise", "Pise", "Florence"), "Rome")
+# )).lancer()
+# lancer le questionnaire n'a pour but que de lancer toutes les questions les unes à la suite des autres
+
+
+# NOUVEAU TRAVAIL
+
+f = open("mon_fichier_txt_avec_question", "r")  # on ouvre le fichier texte avec toutes les questions et réponse
+f_reading = f.read()  # on créé un fichier de lecture
+f_reading_dico = json.loads(f_reading)  # on désérialise : "texte -> dico"
+print(f_reading_dico)
+f.close()  # on a tout copier dans le dico "f_reading_dico" on peut fermer le fichier texte
+
+questions = f_reading_dico["questions"]  # toutes les questions
+categorie = f_reading_dico["categorie"]
+titre = f_reading_dico["titre"]
+
+# on a juste à remplacer le "1" par un "2" pour avoir la 3éme personne au lieu de la 2éme
+# print(questions[1]['titre'])  # question 2
+# print(questions[1]['choix'])  # tous les choix choix de la question 2
+# print(questions[1]['choix'][0][0])  # Choix 1
+# print(questions[1]['choix'][1][0])  # Choix 2
+# print(questions[1]['choix'][2][0])  # Choix 3
+# print(questions[1]['choix'][3][0])  # Choix 4
+# print(questions[1]['choix'][2][0])  # bonne réponse
+
+# à ce moment-là "f_reading_dico" est un dictionnaire dans lequel on a toutes les questions
+# on créé un class "Question_2" équivalent à question pour effectuer le même comportement
+
+# BONNE REPONSES : 2 2 2 1 1 3 3 2 1 3
+Question_list = []
+# print(questions)
+# print(len(questions))
+for i in range(0, 4):  # le nombre de question est : "len(questions)"
+    # pour chacune des questions on veut définir quelle est la bonne réponse
+    Bonne_rep = None  # on initialise ce paramètre pour chacune des questions
+    print(Bonne_rep)
+    print("i : " + str(i))
+    print("les choix de reponse pour la question i sont : " + str(
+        questions[i]['choix']))  # correspond à la liste des "[]" qui représente les choix et un booléean
+    for j in range(0, len(questions[i]['choix'])):
+        #  print("j :" + str(j))
+        # print(questions[i]['choix'][j][1])
+        if questions[i]['choix'][j][1]:  # si on rencontre le "True" on retient le numéro dans les différentes réponses
+            print("la place du True dans les réponse est : " + str(j))
+            Bonne_rep = questions[i]['choix'][j][0]
+            print("la bonne réponse associée est : " + Bonne_rep)
+
+    # on contruit la question, maintenant que l'on a la bonne réponse
+    Question_i = Question(questions[i]['titre'], (
+        questions[i]['choix'][0][0],
+        questions[i]['choix'][1][0],
+        questions[i]['choix'][2][0],
+        questions[i]['choix'][3][0]), Bonne_rep)
+    Question_list.append(Question_i)  # il ne faut pas oublier de passer par un paramètre "question_i" qui correspond à
+    # une question donné. Le paramètre "question" correspond lui à une classe
+
+Questionnaire(  # on peut lui ajouter un tuple ou une liste indiférement. On lui donne la liste des questions définie
+    # plus haut grâce à une boucle "for"
+    Question_list
 ).lancer()
-
-
